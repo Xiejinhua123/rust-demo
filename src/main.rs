857d2cffabbf929f75e899ef8539d54d85b14963
xiejinhua <xiejinhua_9@aliyun.com>
2021-03-28 17:17:50 +0800

    commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
-use std::thread;
-
-use systemstat::{Duration, Platform, System};
-
+extern crate http;
 extern crate systemstat;
 
+use std::{collections::HashMap, fs, result, thread};
+
+use http::{Request, Response, StatusCode, response};
+use reqwest::Response;
+use systemstat::{Duration, Platform, System};
+
 fn main() {
+    ball();
+    let request = Request::get("https://kaijiang.500.com/shtml/ssq/03001.shtml")
+      .body(()).unwrap();
+    let (parts, body) = request.into_parts();
+    println!("{:?}", parts.extensions);
+    Response::into_body(request);
+}
+
+// ball 获取数据
+fn ball(){
+    let text = fs::read_to_string("D:\\a.txt").unwrap();
+    let split = text.split("</a>");
+    for str in split {
+        let url = substr(str, 9, 46);
+        println!("{:?}", url);
+    }
+}
+
+// substr 截取字符串
+fn substr(s: &str, start: usize, length: usize) -> String {
+    s.chars().skip(start).take(length).collect()
+}
+
+// system_monitor 系统监控
+fn system_monitor() {
     let sys = System::new();
 
     match sys.cpu_temp() {
@@ -24,5 +52,4 @@ fn main() {
     }
 
     println!("myname -- xiejinhua");
-
 }
