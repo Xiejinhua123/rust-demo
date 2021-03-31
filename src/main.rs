1107140d964a0c1ad68139a374b7b8e2bec3e1e1
xiejinhua <xiejinhua_9@aliyun.com>
2021-03-31 15:27:00 +0800

    修改代码执行

--- src/main.rs
+++ src/main.rs
@@ -3,41 +3,61 @@ extern crate systemstat;
 extern crate futures;
 extern crate hyper;
 
-use std::{fs, num::ParseIntError, ops::{Index, Sub}, thread, time};
+use std::{fs::{self, OpenOptions}, io::Write, num::ParseIntError, ops::{Index, Sub}, thread, time};
 
 use systemstat::{Duration, Platform, System};
 
 #[cfg(not(target_arch = "wasm32"))]
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-
-    let var_str = "abcdefg";
-    let index = var_str.find("c").unwrap();
-    let var_str_tmp = substr(&var_str, index, 200);
-    println!("{:?}", var_str_tmp);
-
-
+    let mut file = OpenOptions::new().append(true).open("D:\\data.txt").expect(
+        "cannot open file");
     let text = fs::read_to_string("D:\\a.txt").unwrap();
     let split = text.split("</a>");
     for str in split {
         let url = substr(str, 9, 46);
-        println!("{}", &url);
-        print!("{:?}\t", substr(&url, 35, 5));
+        let mut out = substr(&url, 35, 5) + "\t";
         let body = reqwest::get(url).await?.text().await?;
-        let mut body = body.replace("\t", "").replace("\n", "")
+        let body = body.replace("\t", "").replace("\n", "")
         .replace("\r", "").replace("/", "").replace(" ", "")
-        .replace("\"", "");
-        let index_option = body.find("ball_box01");
-        if index_option.is_none() {
-            println!("无");
-            continue;
-        }else{
-            let index = index_option.unwrap();
-            print!("{}", index);
-            body.truncate(index + 1000);
-            println!("{:?}", body);
+        .replace("\"", "").replace("\\", "").replace("f", "")
+        .replace("g", "").replace("h", "").replace("j", "")
+        .replace("k", "").replace("m", "").replace("n", "")
+        .replace("o", "").replace("p", "").replace("q", "")
+        .replace("t", "").replace("v", "").replace("w", "")
+        .replace("y", "").replace("z", "").replace("{", "")
+        .replace("}", "").replace("!", "").replace("-", "")
+        .replace(":", "").replace("=", "").replace(";", "")
+        .replace("]", "").replace("[", "").replace("+", "")
+        .replace(".", "").replace("'", "");
+        let ball = substr(&body, 125000, 1000);
+        if ball.len() > 0{
+            let ball_split = ball.split("<ul>");
+            for ball_li in ball_split {
+                let li_index = ball_li.find("<liclassball");
+                if li_index.is_none(){
+                    continue;
+                }
+                let li_split = ball_li.split("<liclassball");
+                for li_str in li_split {
+                    let red_str = li_str.find("_red>");
+                    if !red_str.is_none(){
+                        let red = li_str.replace("_red>", "").replace("<li>", "");
+                        out += &(red + "\t");
+                    }
+
+                    let blue_str = li_str.find("_blue>");
+                    if !blue_str.is_none(){
+                        let blue = li_str.replace("_blue>", "").replace("<li>", "");
+                        out += &blue;
+                    }
+                }
+            }
         }
-        break;
+        println!("{:?}", out);
+        file.write_all(out.as_bytes()).expect("write failed");
+        file.write_all("\n".as_bytes()).expect("write failed");
+        thread::sleep(time::Duration::from_millis(1000));
     }
     Ok(())
 }
